8e7087a4a8bba33e68d6733fa9bf779175bd8f70
SandyboyBot <killsandeep099@gmail.com>
2026-03-30 19:41:32 +0530

    bot.py

---
+++ bot.py
@@ -0,0 +1,38 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes, CommandHandler
+import yt_dlp
+
+TOKEN = os.getenv("8195590538:AAFTcHzxq8gORIYQ-7_oZtDrHKZcdLFU-LE")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Link pampandi 📥")
+
+async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text
+
+    ydl_opts = {
+        'outtmpl': 'video.%(ext)s',
+        'format': 'mp4'
+    }
+
+    try:
+        await update.message.reply_text("Downloading...")
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+        with open("video.mp4", "rb") as video:
+            await update.message.reply_video(video)
+
+        os.remove("video.mp4")
+
+    except:
+        await update.message.reply_text("Failed ❌")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT, download))
+
+app.run_polling()
